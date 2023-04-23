407320f9a1bcf69068ece898d2ebe0e98c7730a1
Burcu <burju7@yahoo.com>
2023-04-23 15:40:16 +0300

    completed assingment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+(dodger.style.backgroundColor = "#FF69B4"), (dodger.style.bottom = "0px");
+// dodger.style.left = "0px";
+
+// document.addEventListener("keydown", function (event) {
+//   if (event.key === "ArrowLeft") {
+//     const leftNumbers = dodger.style.left.replace("px", "");
+//     const left = parseInt(leftNumbers, 10);
+//     dodger.style.left = `${left - 1}px`;
+//   }
+// });
+
+document.addEventListener("keydown", function (e) {
+  if (e.key == "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (e.key == "ArrowRight") {
+    moveDodgerRight();
+  }
+});
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left >= 179 && left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
